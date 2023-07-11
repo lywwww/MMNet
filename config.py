60be502d019f648f9066b60cf87ef40c09932cf4
lywwww <51897422+lywwww@users.noreply.github.com>
2023-07-11 22:25:41 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,6 @@ import os
 
 
 class Config(object):
-    """
-    define a class to store parameters,
-    the input should be feature mat of training and testing
-    """
-
     def __init__(self):
         # Trainging
         self.learning_rate = 0.001
@@ -16,17 +11,7 @@ class Config(object):
 
         self.epochs = 100
         self.batch_size = 128
-        self.batch_size_per_class = 40  # if equal sampling
-        # dropout keep prob for fully connected layers
-        self.dropout_keep_prob = 0.8
-        # dropout keep prob for convolutional layers
-        self.dropout_keep_prob_conv = 0.8
-
         self.evaluate_every = 50
-
-        self.n_dim = 129  # frequency dimension
-        self.n_time = 29  # time dimension
-        self.n_channel = 3  # channel dimension
         self.n_class = 5  # Final output classes
 
         self.result = ''
